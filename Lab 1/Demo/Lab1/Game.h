6c38c1065bc6b1420563200b53df93d403b7d076
tayyab-07 <tayyabhussain2001@outlook.com>
2023-02-09 16:52:56 +0000

    Lab 1 complete

---
+++ Lab 1/Demo/Lab1/Game.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "Common.h"
+#include "GameWindow.h"
+
+// Classes used in game
+class CShader;
+class CShaderProgram;
+class CHighResolutionTimer;
+
+class Game {
+private:
+	void Initialise();
+	void Update();
+	void Render();
+
+	CShaderProgram *m_pShaderProgram;
+	CHighResolutionTimer *m_pTimer;
+	glm::mat4 *m_pModelMatrix;
+	glm::mat4 *m_pViewMatrix;
+	glm::mat4 *m_pProjectionMatrix;
+	GLuint m_uiVAO;	// A vertex array object (to wrap VBOs)
+
+public:
+	Game();
+	~Game();
+	static Game& GetInstance();
+	LRESULT ProcessEvents(HWND window,UINT message, WPARAM w_param, LPARAM l_param);
+	void SetHinstance(HINSTANCE hinstance);
+	WPARAM Execute();
+
+	void Game::DrawTriangle(glm::vec3 t);
+	void Game::DrawTriangleStack(glm::vec3 s);
+	float m_spacing;
+
+private:
+	void GameLoop();
+	GameWindow gameWindow;
+	HINSTANCE hHinstance;
+
+};
